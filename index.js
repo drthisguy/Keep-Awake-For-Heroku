a7655b5002d89755fea47ebded9b7168679080bb
drthisguy <page.c.tyler@gmail.com>
2020-07-29 11:20:45 -0400

    added landing screen with quit and run options.

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const inquirer = require("inquirer"),
-    // colors = require("console-colors-2"),
+    colors = require("console-colors-2"),
     axios = require("axios");
-
+    
 'use strict';
+const line = new inquirer.Separator();
 
 // prompt users: 
 (() => {
-    const separator = new inquirer.Separator();
-  
-    return inquirer.prompt([
-      {
-      type: "input",
-      name: "site",
-      message: "Type in the URL for the website you would like to keep active"
-      },
-      {
-      type: "list",
-      name: "schedule",
-      message: "On which schedule would you like to keep your site loaded to the server?\n",
-      choices: ["Weekday Working Hours (8am-5pm)", separator,"Full Weekdays (8am-8pm)", separator, "Weekdays (24hrs/day)", separator, "24hr/7days a Week\n"]
+    console.clear();
+    return inquirer.prompt(
+        {
+        type: "rawlist",
+        name: "program",
+        prefix: '*',
+        message: `${colors.fg.green}Keep Heroku Alive.${colors.sp.reset} \n${line}\n Welcome`,
+        choices: ["Start pinging a new page?", `${colors.fg.red}Quit Keep Alive${colors.sp.reset}`]
+        }
+    )
+  })().then( ({ program }) => {
+      if (program === "Start pinging a new page?") {
+        promptSchedule();
       }
-    ])
-  })().then( answers => scheduleCalls(answers))
+        else {
+            console.log(`${colors.fg.blue}Have a great day!${colors.sp.reset}`);
+            process.exit([0]);
+        }
+    })
   .catch( err => console.log(err)) 
 
 const scheduleCalls = ({ schedule, site }) => {
@@ -30,13 +34,13 @@ const scheduleCalls = ({ schedule, site }) => {
     switch (schedule) {
         case "Weekday Working Hours (8am-5pm)":
 
-        conditions = "(now.getDay() > 0 && now.getDay() < 6) && (now.getHours() > 8 && now.getHours() < 17)"
+        conditions = "(now.getDay() > 0 && now.getDay() < 6) && (now.getHours() > 7 && now.getHours() < 17)"
         runSchedule(site, conditions)
         break;
 
         case "Full Weekdays (8am-8pm)":
 
-        conditions = "(now.getDay() > 0 && now.getDay() < 6) && (now.getHours() > 8 && now.getHours() < 20)"
+        conditions = "(now.getDay() > 0 && now.getDay() < 6) && (now.getHours() > 7 && now.getHours() < 20)"
         runSchedule(site, conditions)
         break;
         
@@ -67,4 +71,23 @@ const runSchedule = (url, conditions) => {
     setInterval(() => {
         loadSite()
     }, 1740000);
+}, 
+
+promptSchedule = () => {
+
+    return inquirer.prompt([
+        {
+        type: "input",
+        name: "site",
+        message: "Type in the URL for the website you would like to keep active"
+        },
+        {
+        type: "list",
+        name: "schedule",
+        message: "On which schedule would you like to keep your site loaded to the server?\n",
+        choices: ["Weekday Working Hours (8am-5pm)", line,"Full Weekdays (8am-8pm)", line, "Weekdays (24hrs/day)", line, "24hr/7days a Week\n"]
+        }
+      ])
+    .then( answers => scheduleCalls(answers))
+    .catch( err => console.log(err)) 
 }
