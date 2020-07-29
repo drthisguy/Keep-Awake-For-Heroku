a5d5ead4233310952b36d8bc3dfa6cdd7445dbc6
drthisguy <page.c.tyler@gmail.com>
2020-07-29 11:54:40 -0400

    color and ui updates. try/catch for axios call.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const line = new inquirer.Separator();
         choices: ["Start pinging a new page?", `${colors.fg.red}Quit Keep Alive${colors.sp.reset}`]
         }
     )
-  })().then( ({ program }) => {
+  })().then(({ program }) => {
       if (program === "Start pinging a new page?") {
         promptSchedule();
       }
@@ -61,16 +61,19 @@ const runSchedule = (url, conditions) => {
 
     const loadSite = async () => {
         const now = new Date();
+
+        try {
         if (eval(conditions)) {
             
             await axios.get(url)
-            console.log(`${url} pinged successfully on ${now.toLocaleDateString()} at ${now.toLocaleTimeString()}`)
+            console.log(`${colors.fg.green+url+colors.sp.reset} pinged successfully on ${now.toLocaleDateString()} at ${now.toLocaleTimeString()}`)
         }
+      } catch (err) {console.log(err)}
     }
-
+    loadSite()
     setInterval(() => {
         loadSite()
-    }, 1740000);
+    }, 5000);
 }, 
 
 promptSchedule = () => {
@@ -79,10 +82,10 @@ promptSchedule = () => {
         {
         type: "input",
         name: "site",
-        message: "Type in the URL for the website you would like to keep active"
+        message: "Type in the URL for the website you would like to keep active\n"
         },
         {
-        type: "list",
+        type: "rawlist",
         name: "schedule",
         message: "On which schedule would you like to keep your site loaded to the server?\n",
         choices: ["Weekday Working Hours (8am-5pm)", line,"Full Weekdays (8am-8pm)", line, "Weekdays (24hrs/day)", line, "24hr/7days a Week\n"]
